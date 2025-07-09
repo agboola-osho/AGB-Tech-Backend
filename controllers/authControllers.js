05f0b1b63ca0aa679a9178b3279ba142f159cf17
Abdulrahman Agboola-Osho <111765959+agboola-osho@users.noreply.github.com>
2025-07-09 06:52:24 +0100

    Update authControllers.js

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -116,8 +116,8 @@ const email = async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASS,
